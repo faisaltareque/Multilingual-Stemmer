1321bbd119af111a7628d8260cacfbc9691b98cc
Faisal Tareque <faisaltareque98@gmail.com>
2023-10-05 19:15:34 +0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MultilingualStemmer",
+    version="0.1",
+    packages=find_packages(include=['MultilingualStemmer', 'MultilingualStemmer.*']),
+    install_requires=[
+
+    ],
+)
